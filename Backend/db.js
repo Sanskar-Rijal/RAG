e3b07bda04b899b4209d152bf3cfa7936cef8efe
Sanskar-Rijal <rijalsanskar@gmail.com>
2026-01-15 22:27:18 +0545

    dfasda

---
+++ Backend/db.js
@@ -0,0 +1,48 @@
+import { MongoClient } from "mongodb";
+
+//connect to our database
+const DB = process.env.DATABASE.replace(
+  "<PASSWORD>",
+  process.env.DATABASE_PASSWORD
+);
+
+const DB_NAME = "Rchat";
+
+let client;
+let db;
+
+//connect to the database
+export async function connectToRag() {
+  if (db) return db; //Returns the existing connection if already connected
+  try {
+    client = new MongoClient(DB);
+    //connect client to the server
+    await client.connect();
+    db = client.db(DB_NAME);
+    console.log(`RAG MongoDB connected: ${DB_NAME}`);
+    return db;
+  } catch (err) {
+    console.error("Failed to connect to the database", err);
+    throw err;
+  }
+}
+
+async function getDB() {
+  if (!db) {
+    await connectToRag();
+  }
+  return db;
+}
+
+//get the collection
+export async function getCollection(collectionName) {
+  const database = await getDB();
+  return database.collection(collectionName);
+}
+
+//Close Connection
+export async function closeConnection() {
+  if (client) {
+    await client.close();
+  }
+}
--- Backend/server.js
+++ Backend/server.js
@@ -1,23 +1,23 @@
 import dotenv from "dotenv";
-import mongoose from "mongoose";
+// import mongoose from "mongoose";
 import app from "./app.js";
 
 dotenv.config({ path: "./config.env" });
 
 //connect to our database
-const DB = process.env.DATABASE.replace(
-  "<PASSWORD>",
-  process.env.DATABASE_PASSWORD
-);
+// const DB = process.env.DATABASE.replace(
+//   "<PASSWORD>",
+//   process.env.DATABASE_PASSWORD
+// );
 
-mongoose
-  .connect(DB)
-  .then(() => {
-    console.log("DB connection successful");
-  })
-  .catch((err) => {
-    console.log("DB connection error:", err);
-  });
+// mongoose
+//   .connect(DB)
+//   .then(() => {
+//     console.log("DB connection successful");
+//   })
+//   .catch((err) => {
+//     console.log("DB connection error:", err);
+//   });
 
 //start the server
 const port = process.env.PORT || 9000;
